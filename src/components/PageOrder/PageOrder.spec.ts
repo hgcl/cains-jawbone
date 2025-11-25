017cedb719edef43ced03182c487641b6232dc9b
Clara Le <hey@clarale.com>
2025-11-25 14:26:25 +0100

    test: add test to `PageOrder` component

---
+++ src/components/PageOrder/PageOrder.spec.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect, vi } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import PageOrder from './PageOrder.vue'
+
+describe('PageOrder', () => {
+  Object.assign(navigator, {
+    clipboard: {
+      writeText: () => {},
+    },
+  })
+
+  it('correct value is copied to clipboard', async () => {
+    let spy = vi.spyOn(navigator.clipboard, 'writeText')
+
+    const wrapper = mount(PageOrder, {
+      props: {
+        orderString: '1, 2, 3, 4',
+      },
+    })
+
+    const copyButton = wrapper.find('.page-order__copy-button')
+    await copyButton.trigger('click')
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith('1, 2, 3, 4')
+  })
+})
--- src/components/PageOrder/PageOrder.vue
+++ src/components/PageOrder/PageOrder.vue
@@ -29,7 +29,7 @@ async function copyContent() {
     <label class="page-order__label">Pages order</label>
     <div class="page-order__input_wrapper">
       <input class="page-order__input" :value="pageOrder" readonly />
-      <Button @click="copyContent" :icon="copySvg">Copy</Button>
+      <Button class="page-order__copy-button" @click="copyContent" :icon="copySvg">Copy</Button>
     </div>
   </div>
 </template>
