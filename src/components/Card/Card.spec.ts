1849e5cab2e6d659ff069511887d84009c52bb59
Clara Le <hey@clarale.com>
2025-12-01 15:13:59 +0100

    test: remove outdated `Card` test

--- src/components/Card/Card.spec.ts
+++ src/components/Card/Card.spec.ts
@@ -19,19 +19,4 @@ describe('Card', () => {
     expect(wrapper.find('.card__page').text().toLowerCase()).toContain('page 2')
     expect(wrapper.find('.card__preview').text()).toContain('Some lines flying off page 2')
   })
-
-  it('shows and hide correct buttons', () => {
-    let wrapper = mount(Card, { props: { page: mockPage } })
-
-    // Page belongs to list 1 => show "sort" button
-    expect(wrapper.find('.card__button_sort').exists()).toBe(true)
-    expect(wrapper.find('.card__button_remove').exists()).toBe(false)
-
-    // Page belongs to list 2 => show "remove" button
-    mockPage.list = 2
-    wrapper = mount(Card, { props: { page: mockPage } })
-
-    expect(wrapper.find('.card__button_sort').exists()).toBe(false)
-    expect(wrapper.find('.card__button_remove').exists()).toBe(true)
-  })
 })
