c19cb1752935b1381091cc682eeac7d704e5cf37
Clara Le <hey@clarale.com>
2025-12-01 14:03:51 +0100

    feat: move page arrays into tabs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,29 @@ import Button from '../components/Button/Button.vue'
 
 <template>
   <main>
-    <h1>Cain's Jawbone</h1>
-    <p>
-      In 1934, Torquemada penned a gripping 100-page murder mystery... but there's a twist. The
-      pages have been scrambled, and the story is arranged all wrong.
-    </p>
-    <p>
-      Can you find the correct page order, uncover the secrets hidden within, and solve the chilling
-      murders?
-    </p>
-    <Button :to="'/book'" :inverted="true">Open the book</Button>
+    <div>
+      <h1>Cain's Jawbone</h1>
+      <p>
+        In 1934, Torquemada penned a gripping 100-page murder mystery... but there's a twist. The
+        pages have been scrambled, and the story is arranged all wrong.
+      </p>
+      <p>
+        Can you find the correct page order, uncover the secrets hidden within, and solve the
+        chilling murders?
+      </p>
+      <Button :to="'/book'" :inverted="true">Open the book</Button>
+    </div>
   </main>
 </template>
 
 <style scoped>
 main {
-  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+main > div {
+  max-width: 464px;
   display: flex;
   flex-direction: column;
   gap: var(--gap-l);
@@ -27,10 +34,6 @@ main {
   font-size: var(--font-size-body-l);
   color: var(--color-on-accent);
   background-color: var(--color-accent);
-  border-radius: var(--border-radius);
-}
-main > * {
-  max-width: 464px;
 }
 h1 {
   font-size: var(--font-size-heading-xl);
