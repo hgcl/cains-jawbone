9ab0588617dc50b0bdf1f6c220a3cdce6ecb06af
Clara Le <hey@clarale.com>
2025-12-08 23:46:37 +0100

    refactor: add `NotificationInline` instead of custom code

--- src/components/PageOrder/PageOrder.vue
+++ src/components/PageOrder/PageOrder.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import Button from '../Button/Button.vue'
 import ShowElement from '../ShowElement/ShowElement.vue'
+import NotificationInline from '../NotificationInline/NotificationInline.vue'
 import copySvg from '../../assets/copy-feathericons.svg'
-import alertSvg from '../../assets/alert-triangle-feathericons.svg'
 import { useCopyContent } from './PageOrder.utils'
 import { computed, ref, watch } from 'vue'
 
@@ -15,12 +15,9 @@ const emit = defineEmits<{
 
 const localInput = ref<string>(orderString)
 const copyRef = ref<InstanceType<typeof ShowElement> | null>(null)
-const warningRef = ref<InstanceType<typeof ShowElement> | null>(null)
-const warningMessage = ref<string>()
-
-const warningIcon = computed(() => ({
-  mask: `url("${alertSvg}") no-repeat center`,
-}))
+const errorMessage = ref<string>()
+// Exposes functions from `ShowElement`
+const showElRef = ref<InstanceType<typeof ShowElement> | null>(null)
 
 watch(
   () => orderString,
@@ -40,9 +37,9 @@ function reviewOrderString() {
   // * => pattern can be repeated
   const isValid = /^[0-9,\s]*$/.test(localInput.value)
   if (!isValid) {
-    warningMessage.value =
+    errorMessage.value =
       'Remove the invalid characters to apply the new page order. Only numbers, commas, and spaces are allowed.'
-    warningRef.value?.show()
+    showElRef.value?.show()
     return
   }
 
@@ -56,21 +53,21 @@ function reviewOrderString() {
   // 3. Check if there are duplicates
   const duplicates = numberArray.filter((item, index) => numberArray.indexOf(item) !== index)
   if (duplicates.length > 0) {
-    warningMessage.value = `Remove the following duplicates to apply the new page order: ${duplicates.join(', ')}`
-    warningRef.value?.show()
+    errorMessage.value = `Remove the following duplicates to apply the new page order: ${duplicates.join(', ')}`
+    showElRef.value?.show()
     return
   }
 
   // 4. Check if there are invalid numbers
   const invalidNumbers = numberArray.filter((item) => item > 100 || item < 1)
   if (invalidNumbers.length > 0) {
-    warningMessage.value = `Remove these invalid page numbers: ${invalidNumbers.join(', ')}`
-    warningRef.value?.show()
+    errorMessage.value = `Remove these invalid page numbers: ${invalidNumbers.join(', ')}`
+    showElRef.value?.show()
     return
   }
 
   // 5. If all is valid, emit safely
-  warningRef.value?.hide()
+  showElRef.value?.hide()
   emit('update:orderstring', numberArray)
 }
 
@@ -84,14 +81,15 @@ const { copyContent } = useCopyContent(localInput, copyRef)
     <div class="page-order__input_wrapper">
       <input v-model="localInput" type="text" @keydown.enter="reviewOrderString" />
       <Button @click="reviewOrderString">Apply</Button>
-      <Button class="page-order__copy-button" @click="copyContent" :iconBefore="copySvg"
-        >Copy</Button
-      >
+      <Button class="page-order__copy-button" @click="copyContent" :iconBefore="copySvg">
+        Copy
+      </Button>
     </div>
-    <ShowElement ref="warningRef" class="page-order__warning"
-      ><div :style="warningIcon"></div>
-      {{ warningMessage }}</ShowElement
-    >
+    <ShowElement ref="showElRef">
+      <NotificationInline :status="'danger'">
+        {{ errorMessage }}
+      </NotificationInline>
+    </ShowElement>
   </div>
 </template>
 
@@ -106,35 +104,21 @@ const { copyContent } = useCopyContent(localInput, copyRef)
   /* Necessary for .page-order__copied positioning */
   position: relative;
 }
+
 .page-order__copied {
   position: absolute;
   top: calc(-0.25 * 16px);
   right: calc(0.5 * 16px);
   color: var(--color-accent);
 }
-.page-order__warning {
-  display: flex;
-  gap: var(--gap-s);
-  padding: var(--padding-xs);
-  border-radius: var(--border-radius);
-  border: 1px solid var(--color-foreground-danger);
-  color: var(--color-foreground-danger);
-  background: var(--color-background-danger);
-  font-size: var(--font-size-body-s);
-  font-weight: bold;
-}
-.page-order__warning > div {
-  background-color: var(--color-foreground-danger);
-  width: 24px;
-  height: 24px;
-  transform: scale(80%);
-}
+
 .page-order__label {
   text-transform: uppercase;
   font-weight: bold;
   font-size: var(--font-size-body-s);
   color: var(--color-accent);
 }
+
 .page-order__input_wrapper {
   display: flex;
   justify-content: space-between;
